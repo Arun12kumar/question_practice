6e4e15ea63be24057fc487bc74bbbc574baeb3b7
RukerDevps <86552741+Arun12kumar@users.noreply.github.com>
2025-06-18 07:53:14 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],  base: '/question_practice/', // 👈 Important!
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
